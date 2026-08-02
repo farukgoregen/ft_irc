f3d6ccc8364cf98270651ee77b9f0f33f8c506d3
Mustafa Ersin Şişman <mustafaersinsisman@gmail.com>
2026-08-02 13:03:46 +0300

    part mesaj işlemi düzeltildi

--- Commands.cpp
+++ Commands.cpp
@@ -563,6 +563,10 @@ void Commands::handlePart(Client* client, const std::vector<std::string>& args, 
 	}
 
 	std::string reason = (args.size() > 2) ? args[2] : "Leaving";
+
+	for (int i = 3; i < args.size(); i++)
+		reason += " " + args[i];
+
 	std::string partMsg = ":" + client->getNickname() +
 				"!" + client->getUsername() +
 				"@" + client->getHostname() +
